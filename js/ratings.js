7e8184da940d4a7ccc96a1afd6342063ca2fb75d
micmasaphi <micmasaphi@gmail.com>
2018-02-25 21:33:11 -0500

    Scaling Fixed
    0, 100 scaling set (duh)
    working on transitions

--- js/ratings.js
+++ js/ratings.js
@@ -4,12 +4,12 @@ const margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var svg = d3.select("body").append("svg")
+var myChart = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .style('background','#E7E0CB')
 
-var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var g = myChart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv(dataUrl, function(error, data) {
 		if(error) throw error;
@@ -29,45 +29,54 @@ d3.csv(dataUrl, function(error, data) {
 
     });
 
+	const binCount = Math.floor(Math.sqrt(data.length));
+
+
 	var x = d3.scaleLinear()
+		.domain([0,100])
     	.range([0, width]);
 
     var bins = d3.histogram()
 		.value(function(d) { return d.mc_rating; })
-		.domain(x.domain()) //what is going on HERE?
-		//.thresholds(x.ticks(10))
+		.domain(x.domain())
+		.thresholds(x.ticks(binCount))
 		(data);
 
-	//TODO: SOMETHING'S UP HERE. When I add x.domain() to bins, it throws off
-	//the y scaling...
 	var y = d3.scaleLinear()
 	    .domain([0, d3.max(bins, function(d) { return d.length; })])
 	    .range([height, 0]);
-
-	console.log(bins);
 
     var bar = g.selectAll(".bar")
 	  .data(bins)
 	  .enter().append("g")
 	    .attr("class", "bar")
-	    .attr("transform", function(d) { console.log("x0:" + x(d.x0)); return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+	    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });//return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
 
 	bar.append("rect")
-	    //.attr("x", 1)
-	    .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-	    .attr("height", function(d) { console.log(d.length); return height - y(d.length); });
+	    .attr("x", 1)
+	   	.attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+	    .transition()
+	    	.duration(1000)
+	    	.attr("height", function(d) { return height - y(d.length); })
+	    	
+	    
 
 	/*bar.append("text")
 	    .attr("dy", ".75em")
 	    .attr("y", 6)
 	    .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-	    .attr("text-anchor", "middle");
-	    .text(function(d) { return formatCount(d.length); });*/
+	    .attr("text-anchor", "middle")
+	    .text(function(d) { return d.length; });*/		
 
 	g.append("g")
 	    .attr("class", "axis axis--x")
 	    .attr("transform", "translate(0," + height + ")")
 	    .call(d3.axisBottom(x));
 
-
+    g.append("g")
+    	.attr("class", "axis axis--y")
+    	//.attr("transform", "translate(0," + height + ")")
+    	.call(d3.axisRight(y))
 });
+
+
